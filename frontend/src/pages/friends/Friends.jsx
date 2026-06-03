8289050d5de004a01b27c0094a70ccbe7d495793
sopon <soponchandradevsharma388@gmail.com>
2026-06-03 11:42:09 +0600

    again update

--- frontend/src/pages/friends/Friends.jsx
+++ frontend/src/pages/friends/Friends.jsx
@@ -1,10 +1,57 @@
-import FriendRequest from "../../components/FriendRequest/FriendRequest";
+import { useEffect, useState } from "react";
 
 import MainLayout from "../../layouts/MainLayout";
+
+import API from "../../api/axios";
 
 import "./Friends.css";
 
 const Friends = () => {
+
+  const [users, setUsers] =
+    useState([]);
+
+  const fetchUsers = async () => {
+
+    try {
+
+      const { data } =
+        await API.get(
+          "/friends/users"
+        );
+
+      setUsers(data.users);
+
+    } catch (error) {
+
+      console.log(error);
+    }
+  };
+
+  const handleAddFriend =
+    async (userId) => {
+
+      try {
+
+        await API.post(
+          `/friends/send/${userId}`
+        );
+
+        alert(
+          "Friend Request Sent"
+        );
+
+      } catch (error) {
+
+        console.log(error);
+      }
+    };
+
+  useEffect(() => {
+
+    fetchUsers();
+
+  }, []);
 
   return (
 
@@ -12,7 +59,41 @@ const Friends = () => {
 
       <div className="friends-page">
 
-        <FriendRequest />
+        {users.map((user) => (
+
+          <div
+            className="friend-card"
+            key={user._id}
+          >
+
+            <div className="friend-info">
+
+              <img
+                src={
+                  user.profilePic
+                }
+                alt="profile"
+              />
+
+              <h3>
+                {user.name}
+              </h3>
+
+            </div>
+
+            <button
+              onClick={() =>
+                handleAddFriend(
+                  user._id
+                )
+              }
+            >
+              Add Friend
+            </button>
+
+          </div>
+
+        ))}
 
       </div>
 
