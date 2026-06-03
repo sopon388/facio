52f22c8e3a83ebb9f3d5ed9a92bc8c6ab2a0f558
sopon <soponchandradevsharma388@gmail.com>
2026-06-03 13:12:41 +0600

    again

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import {Link ,useNavigate } from "react-router-dom";
 import API from "../../api/axios";
+import "./Register.css";
 const Register = () => {
   const navigate = useNavigate();
 
